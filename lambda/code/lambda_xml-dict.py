6737f21457af76d8c8418d8472be394fbf0ee9d5
Matheus Mandotti <mathew.mfranco@gmail.com>
2021-01-25 19:14:04 -0300

    Change function name to lambda_handler

--- lambda/code/lambda_xml-dict.py
+++ lambda/code/lambda_xml-dict.py
@@ -2,33 +2,31 @@ import urllib
 import boto3
 import xmltodict
 
+def lambda_handler( event, context ):
 
-def convert_xml_json( event, context ):
-    
-    s3 = boto3.client('s3')
-    
-    # get bucket name
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    print("Bucket: " + bucket)
-    
-    #get the file/key name
-    fineName = urllib.parse.unquote_plus( event['Records'][0]['s3']['object']['key'], encoding = 'utf-8' )
-    print("File Name: " + fineName )
-    
-    try:
-        
-        # Fetch the file from s3
-        response = s3.get_object( Bucket = bucket, Key = fineName )
-        
-        file = response['Body'].read()
-        
-        #return as dict
-        doc = xmltodict.parse( file )
-        print(doc)
-        return doc
-        
-        
-    except Exception as e:
-        print( e )
-        raise e
-    
+	s3 = boto3.client('s3')
+
+	# get bucket name
+	bucket = event['Records'][0]['s3']['bucket']['name']
+	print("Bucket: " + bucket)
+
+	#get the file/key name
+	fineName = urllib.parse.unquote_plus( event['Records'][0]['s3']['object']['key'], encoding = 'utf-8' )
+	print("File Name: " + fineName )
+
+	try:
+
+		# Fetch the file from s3
+		response = s3.get_object( Bucket = bucket, Key = fineName )
+
+		file = response['Body'].read()
+
+		#return as dict
+		doc = xmltodict.parse( file )
+		print(doc)
+		return doc
+
+
+	except Exception as e:
+		print( e )
+		raise e
